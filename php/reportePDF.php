ee6969b2b0488133de4cf7e2d9255d7d266da63c
Yoseb Julian Ospitia Gomez <caleb2515@outlook.es>
2021-12-03 05:45:48 -0500

    6.2 Diseño de reportes actualizado

--- php/reportePDF.php
+++ php/reportePDF.php
@@ -17,13 +17,13 @@
     $pdf->Cell(90,10,'Hora: '.date('H:i:s').'',0);
     $pdf->Ln(15);
     $pdf->SetFont('Times','B',10);
-    $pdf->Cell(25,20,'CODIGO');
-    $pdf->Cell(45,20,'NOMBRE');
+    $pdf->Cell(20,20,'CODIGO');
+    $pdf->Cell(63,20,'NOMBRE');
     $pdf->Cell(35,20,'TIPO');
     $pdf->Cell(28,20,'CANTIDAD');
     $pdf->Cell(35,20,'CLASE');
-    $pdf->Cell(30,20,'MARCA');
-    $pdf->Cell(30,20,'SERIAL');
+    $pdf->Cell(22,20,'MARCA');
+    $pdf->Cell(25,20,'SERIAL');
     $pdf->Cell(30,20,'MODELO');
     $pdf->Cell(30,20,'PRECIO');
     $pdf->Ln(15);
@@ -34,14 +34,14 @@
     if (mysqli_num_rows($result) > 0) {
         while ($reg = mysqli_fetch_assoc($result)) {
             $pdf->SetFont('Times','B',9);
-            $pdf->Cell(25,20, utf8_decode($reg['pCod']),0);
+            $pdf->Cell(20,20, utf8_decode($reg['pCod']),0);
             $pdf->SetFont('Times','',9);
-            $pdf->Cell(45,20, utf8_decode($reg['pNom']),0);
+            $pdf->Cell(63,20, utf8_decode($reg['pNom']),0);
             $pdf->Cell(35,20, utf8_decode($reg['pTip']),0);
             $pdf->Cell(28,20, '     '.($reg['pCant']).'     unid',0);
             $pdf->Cell(35,20, utf8_decode($reg['pClas']),0);
-            $pdf->Cell(30,20, utf8_decode($reg['pMarc']),0);
-            $pdf->Cell(30,20, utf8_decode($reg['pSer']),0);
+            $pdf->Cell(22,20, utf8_decode($reg['pMarc']),0);
+            $pdf->Cell(25,20, utf8_decode($reg['pSer']),0);
             $pdf->Cell(30,20, utf8_decode($reg['pMod']),0);
             $pdf->Cell(30,20, ($reg['pPrecio']),0);
             $pdf->Ln(13);
